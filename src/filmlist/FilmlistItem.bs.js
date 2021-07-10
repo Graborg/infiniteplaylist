c521bd475820e4fd9cf19e03bb59e71a3caa53e4
Mikael Gråborg <mikael.graborg@iteam.se>
2021-07-10 17:31:17 -0500

    feat: add checkbox

--- src/filmlist/FilmlistItem.bs.js
+++ src/filmlist/FilmlistItem.bs.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var Curry = require("rescript/lib/js/curry.js");
 var React = require("react");
 
 function creatorToString(creator) {
@@ -15,23 +16,85 @@ function FilmlistItem(Props) {
   var id = Props.id;
   var lastElement = Props.lastElement;
   var selected = Props.selected;
-  return React.createElement("div", {
-              key: String(id),
-              style: {
-                borderBottom: lastElement ? "" : "1px #cecece solid",
-                display: "flex",
-                margin: "0 10px",
-                minHeight: "56px",
-                padding: "21px 5px 13px",
-                paddingLeft: selected ? "20px" : "10px",
-                justifyContent: "space-between",
-                boxSizing: "border-box"
-              }
-            }, React.createElement("p", undefined, selected ? "🤠" + " " : "", film.name, selected ? " " + "🤠" : ""), React.createElement("p", {
-                  style: {
-                    border: "black 5px"
-                  }
-                }, film.creator ? "Ferma" : "karmi"));
+  var match = React.useState(function () {
+        return false;
+      });
+  var setMouseOver = match[1];
+  var match$1 = React.useState(function () {
+        return false;
+      });
+  var setCheck = match$1[1];
+  var isChecked = match$1[0];
+  if (match[0] || isChecked) {
+    return React.createElement("div", {
+                key: String(id),
+                className: "film-item inputGroup",
+                style: {
+                  margin: "0 10px",
+                  padding: "0"
+                },
+                onMouseEnter: (function (param) {
+                    return Curry._1(setMouseOver, (function (param) {
+                                  return true;
+                                }));
+                  }),
+                onMouseLeave: (function (param) {
+                    return Curry._1(setMouseOver, (function (param) {
+                                  return false;
+                                }));
+                  })
+              }, React.createElement("input", {
+                    id: String(id) + "input",
+                    checked: isChecked,
+                    type: "checkbox",
+                    onChange: (function (param) {
+                        console.log("Hej");
+                        return Curry._1(setCheck, (function (prev) {
+                                      return !prev;
+                                    }));
+                      })
+                  }), React.createElement("label", {
+                    style: {
+                      backgroundColor: "white",
+                      borderBottom: lastElement ? "" : "1px #cecece solid",
+                      display: "flex",
+                      minHeight: "56px",
+                      padding: "21px 0 13px",
+                      paddingLeft: "20px",
+                      justifyContent: "space-between",
+                      boxSizing: "border-box"
+                    },
+                    htmlFor: String(id) + "input"
+                  }, film.name));
+  } else {
+    return React.createElement("div", {
+                key: String(id),
+                className: "film-item inputGroup",
+                style: {
+                  borderBottom: lastElement ? "" : "1px #cecece solid",
+                  display: "flex",
+                  margin: "0 10px",
+                  minHeight: "56px",
+                  padding: "21px 5px 13px",
+                  justifyContent: "space-between",
+                  boxSizing: "border-box"
+                },
+                onMouseEnter: (function (param) {
+                    return Curry._1(setMouseOver, (function (param) {
+                                  return true;
+                                }));
+                  }),
+                onMouseLeave: (function (param) {
+                    return Curry._1(setMouseOver, (function (param) {
+                                  return false;
+                                }));
+                  })
+              }, React.createElement("p", undefined, selected ? "🤠" + " " : "", film.name, selected ? " " + "🤠" : ""), React.createElement("p", {
+                    style: {
+                      border: "black 5px"
+                    }
+                  }, film.creator ? "Ferma" : "karmi"));
+  }
 }
 
 var make = FilmlistItem;
--- src/filmlist/FilmlistItem.res
+++ src/filmlist/FilmlistItem.res
@@ -1,5 +1,9 @@
 open Todoist
 
+type mouseOver =
+  | MouseIsOver
+  | MouseNotOver
+
 let creatorToString = (creator: Todoist.creator) =>
   switch creator {
   | Karmi => "karmi"
@@ -8,26 +12,66 @@ let creatorToString = (creator: Todoist.creator) =>
 
 @react.component
 let make = (~film: Todoist.film, ~id, ~lastElement: bool, ~selected: bool) => {
-  <div
-    key={Belt.Int.toString(id)}
-    style={ReactDOMStyle.make(
-      ~display="flex",
-      ~padding="21px 5px 13px",
-      ~margin="0 10px",
-      ~paddingLeft=selected ? "20px" : "10px",
-      ~borderBottom=lastElement ? "" : "1px #cecece solid",
-      ~justifyContent="space-between",
-      ~boxSizing="border-box",
-      ~minHeight="56px",
-      (),
-    )}>
-    <p>
-      {selected ? React.string(j`🤠` ++ " ") : React.string("")}
-      {React.string(film.name)}
-      {selected ? React.string(" " ++ j`🤠`) : React.string("")}
-    </p>
-    <p style={ReactDOMStyle.make(~border="black 5px", ())}>
-      {React.string(creatorToString(film.creator))}
-    </p>
-  </div>
+  let (isMouseOver, setMouseOver) = React.useState(_ => false)
+  let (isChecked, setCheck) = React.useState(_ => false)
+
+  {
+    isMouseOver || isChecked
+      ? <div
+          key={Belt.Int.toString(id)}
+          onMouseEnter={_ => setMouseOver(_ => true)}
+          onMouseLeave={_ => setMouseOver(_ => false)}
+          className="film-item inputGroup"
+          style={ReactDOMStyle.make(~padding="0", ~margin="0 10px", ())}>
+          <input
+            checked=isChecked
+            onChange={_ => {
+              Js.log("Hej")
+              setCheck(prev => !prev)
+            }}
+            id={Belt.Int.toString(id) ++ "input"}
+            type_="checkbox"
+          />
+          <label
+            style={ReactDOMStyle.make(
+              ~display="flex",
+              ~padding="21px 0 13px",
+              ~paddingLeft="20px",
+              ~justifyContent="space-between",
+              ~boxSizing="border-box",
+              ~minHeight="56px",
+              ~borderBottom=lastElement ? "" : "1px #cecece solid",
+              ~backgroundColor="white",
+              (),
+            )}
+            htmlFor={Belt.Int.toString(id) ++ "input"}>
+            {React.string(film.name)}
+          </label>
+        </div>
+      : <div
+          key={Belt.Int.toString(id)}
+          onMouseEnter={_ => setMouseOver(_ => true)}
+          onMouseLeave={_ => setMouseOver(_ => false)}
+          className="film-item inputGroup"
+          style={ReactDOMStyle.make(
+            ~display="flex",
+            // ~padding="21px 0 13px",
+            ~padding="21px 5px 13px",
+            ~margin="0 10px",
+            ~borderBottom=lastElement ? "" : "1px #cecece solid",
+            ~justifyContent="space-between",
+            ~boxSizing="border-box",
+            ~minHeight="56px",
+            (),
+          )}>
+          <p>
+            {selected ? React.string(j`🤠` ++ " ") : React.string("")}
+            {React.string(film.name)}
+            {selected ? React.string(" " ++ j`🤠`) : React.string("")}
+          </p>
+          <p style={ReactDOMStyle.make(~border="black 5px", ())}>
+            {React.string(creatorToString(film.creator))}
+          </p>
+        </div>
+  }
 }
